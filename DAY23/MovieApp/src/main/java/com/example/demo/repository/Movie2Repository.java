b85291186c700cd626c15cc21c10cc11d6df70e4
HariniSabarimalai41 <120249233+HariniSabarimalai41@users.noreply.github.com>
2023-10-16 10:17:45 +0530

    Add files via upload

---
+++ DAY23/MovieApp/src/main/java/com/example/demo/repository/Movie2Repository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.demo.model.Movie;
+import com.example.demo.model.Movie2;
+
+
+//@Repository
+public interface Movie2Repository extends JpaRepository<Movie2, Integer>{
+
+
+}
